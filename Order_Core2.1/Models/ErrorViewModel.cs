f01a71c8e75712b617e588aa1ad19078db064837
jiaohaojiegithub <15949529192@163.com>
2018-07-13 16:16:48 +0800

    添加项目文件。

---
+++ Order_Core2.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Order_Core2._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
