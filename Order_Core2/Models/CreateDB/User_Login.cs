3520c266b635ccbc54d64c1219a6821fd4bbc563
豪杰 焦 <2351592225@qq.com>
2018-07-14 23:34:57 +0800

    添加User_Login表

---
+++ Order_Core2/Models/CreateDB/User_Login.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Order_Core2.Models.CreateDB
+{
+    [Category("用户表")]
+    public class User_Login
+    {
+        [Description("User_Login_ID 主键 ID标识 数值递增")]
+        public int User_Login_ID { get; set; }
+        [Description("User_Login_Name 用户名")]
+        public string User_Login_Name { get; set; }
+        [Description("User_Login_EmailAddress 邮箱地址 扩展登录用户")]
+        [DataType(DataType.EmailAddress)]
+        public string User_Login_EmailAddress { get; set; }
+        [Description("User_Login_PassWord 用户密码")]
+        public string User_Login_PassWord { get; set; }
+        [Description("User_Login_Sex 性别(男，女)")]
+        public string User_Login_Sex { get; set; }
+        [Description("User_Login_Right 权限(管理员OR普通用户)")]
+        public int User_Login_Right { get; set; }
+        [Description("User_Login_State 用户状态(0为假 离线，1为真 在线)")]
+        public bool User_Login_State { get; set; }
+        [Description("User_Login_Guid 唯一标识 数值随机")]
+        public Guid User_Login_Guid { get; set; }
+        [Description("User_Login_CreatDT 创建时间")]
+        public DateTime User_Login_CreatDT { get; set; }
+
+    }
+}
---
+++ Order_Core2/Models/DBMYBLOGAPIContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Order_Core2.Models.CreateDB;
+
+namespace Order_Core2.Models
+{
+    public class DBMYBLOGAPIContext:DbContext
+    {
+        public DBMYBLOGAPIContext(DbContextOptions<DBMYBLOGAPIContext> options) : base(options)
+        {
+            //连接到数据服务器
+        }
+        DbSet<User_Login> User_Login { get; set; }
+        #region 建立数据库数据关联
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User_Login>(entity =>
+            {
+                entity.HasKey(e => e.User_Login_ID);//主键
+                entity.Property(e => e.User_Login_ID).ValueGeneratedOnAdd();//在生成的值添加
+
+                entity.Property(e => e.User_Login_Name).HasMaxLength(20);
+
+                entity.Property(e => e.User_Login_PassWord).HasMaxLength(20);
+
+                entity.Property(e => e.User_Login_Right).IsRequired();//不能为空
+
+                entity.Property(e => e.User_Login_Sex).HasMaxLength(5);
+
+                entity.Property(e => e.User_Login_State).IsRequired();
+
+                entity.Property(e => e.User_Login_Guid).HasDefaultValueSql("NEWID()");
+
+                entity.Property(e => e.User_Login_CreatDT).HasDefaultValueSql("GETDATE()");
+
+                entity.Property(e => e.User_Login_EmailAddress).HasColumnType("varchar(200)");
+            });
+        }
+        #endregion
+    }
+}
